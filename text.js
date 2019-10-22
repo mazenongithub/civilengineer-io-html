2f8de5372a67e41ec9e88653d4a9a9195c22dc72
mazenongithub <mazen@egeotechnical.com>
2019-10-22 10:14:53 -0700

    set up for dynamic content

--- text.js
+++ text.js
@@ -1,18 +1,42 @@
-function openmenusmall() {
+function homepage() {
+    const myapps = this.myapps();
+    let homepage = [];
+    let appdisplay = [];
+    myapps.map(myapp => {
+        appdisplay.push(this.createapp(myapp))
+    })
 
     if (this.state.width > 720) {
+        if (this.state.menu === 'open') {
+            homepage.push(<div className="general-app-container content-container regularHeight bold-font">
+                Delivering Free Open Source Applications to the internet, IOS, and Google Play Markets
+                  </div>)
 
-        if (this.state.menu === "open") {
-            return (this.openmenularge())
-        } else {
-            return (this.closedmenularge())
+            homepage.push(this.appdisplay())
+
+
+        } else if (this.state.menu === 'closed') {
+
+            homepage.push(<div className="general-app-container span-all regularHeight bold-font">
+                Delivering Free Open Source Applications to the internet, IOS, and Google Play Markets
+      </div>)
+            homepage.push(this.appdisplay())
+
         }
+        return homepage;
 
     } else {
+        return (
+            <div className="general-flex">
+                <div className="flex-one general-font">
+                    <div className="general-app-container regularHeight">
+                        Delivering Free Open Source Applications to the internet, IOS, and Google Play Markets
+          </div>
 
-        return (this.openmenusmall())
+                    {appdisplay}
+
+                </div>
+            </div>
+        )
     }
-
-
-
 }
